6514307eceaf6c4b797240e8690e2db03eab2c01
king <mto0006@auburn.edu>
2025-09-17 05:44:47 -0500

    Add stars with parallax effects

--- src/components/AnimatedBackground.jsx
+++ src/components/AnimatedBackground.jsx
@@ -1,11 +1,14 @@
 import '../styles/animations.css'; // Import animations
+import '../styles/parallax.css'; // Import parallax styles
 
 const AnimatedBackground = () => (
+  // Parallax Pixel Background Animation
   <section className="animated-background">
     <div id="stars1"></div>
     <div id="stars2"></div>
     <div id="stars3"></div>
   </section>
+  // End of Parallax Pixel Background Animation
 );
 
 export default AnimatedBackground;
--- src/components/Links.jsx
+++ src/components/Links.jsx
@@ -22,23 +22,3 @@ export default Links;
 
 
 
-
-// import './links.css';
-
-// function Links() {
-//   return (
-//     <div id="links">
-//       <a className="link" href="http://github.com/kingmidas-hack" target="_blank">
-//         <i className="fab fa-github">&nbsp;</i>Github
-//       </a>
-//       <a className="link" href="https://www.linkedin.com/in/midasoden/" target="_blank">
-//         <i className="fab fa-linkedin">&nbsp;</i>LinkedIn
-//       </a>
-//       <a className="link" href="https://beta.crunchyroll.com/crunchylists/eeb252c9-fb45-483a-b119-3ba075445157" target="_blank">
-//         <i className="fa fa-play-circle">&nbsp;</i>MyAnimeList
-//       </a>
-//     </div>
-//   );
-// }
-
-// export default Links;
