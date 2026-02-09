07891d29198bed954bbe0aec20d517daf8f85330
king <mto0006@auburn.edu>
2026-02-09 06:52:23 -0600

    Modify Projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,15 +16,45 @@ function Projects() {
           marginTop: "40px"
       }}>
         <ProjectCard
+          title="thoughtSpace"
+          description="A borderless, anonymous social media app where users leave ephemeral “notes” in a shared infinite space."
+          github="https://github.com/thoughtSpace-app/thoughtSpace"
+        />
+
+        <ProjectCard
+          title="Auburn Coursework"
+          description="An overview of some of the programming courses taken at Auburn University."
+          github="https://github.com/midas-kingme/Auburn-Courses"
+        />
+
+        <ProjectCard
+          title="Course Management System"
+          description="CMS for a group project showcases online course catalog."
+          github="https://github.com/cadomani/course-management-system"
+        />
+
+        <ProjectCard
           title="ChatBookie"
           description="A chatbot that predicts sports outcomes using machine learning."
-          github="https://github.com/midas-kingme"
+          github="https://github.com/midas-kingme/ChatBookie"
         />
 
         <ProjectCard
           title="Opportunity Protocol"
-          description="Edit this"
-          github="https://github.com/midas-kingme"
+          description="A decentralized, non-custodial social graph that implements unique, on-chain social interaction mechanisms ocial media interactions."
+          github="https://github.com/midas-kingme/opportunity-client"
+        />
+
+        <ProjectCard
+          title="Pure Life Pools"
+          description="Website that showcases custom pool designs and expert craftsmanship."
+          github="https://github.com/midas-kingme/pure-life-pools"
+        />
+
+        <ProjectCard
+          title="PeerNative"
+          description="An Ai powered mobile application facilitating all utilities you need to improve your fluency in a language."
+          github="https://github.com/midas-kingme/peernative"
         />
       </div>
     </div>
